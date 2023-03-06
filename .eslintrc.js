ebd2d99b500f95036c11ff006ce63982008fe2f6
Marcin Saja <marcin.saja.webmaster@gmail.com>
2023-03-06 13:57:18 +0100

    chore(in-2527): eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+const { resolve } = require('path');
+
+module.exports = {
+  env: {
+    jest: true,
+    node: true,
+    browser: true,
+    es6: true,
+    commonjs: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: [
+      resolve(__dirname, './tsconfig.json'),
+      resolve(__dirname, './tests/e2e/tsconfig.json'),
+    ],
+    tsconfigRootDir: __dirname,
+    extraFileExtensions: ['.vue'],
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  extends: [
+    '@vue-storefront/eslint-config-base',
+    '@vue-storefront/eslint-config-typescript',
+    '@vue-storefront/eslint-config-import',
+    '@vue-storefront/eslint-config-vue',
+    '@vue-storefront/eslint-config-jest',
+  ],
+  globals: {
+    "__ENV": "readonly",
+  },
+  rules: {
+    "@typescript-eslint/no-floating-promises": "off",
+    "jest/expect-expect": [
+      "error",
+      {
+        "assertFunctionNames": ["expect", "getByRole", "getByTestId", "getByText"],
+      }
+    ],
+    "no-plusplus": "off",
+  },
+  overrides: [
+    {
+      "files": ["tests/e2e/**/*"],
+      "rules": {
+        "jest/expect-expect": "off",
+        "promise/catch-or-return": "off", // conflicts with Cypress.Chainable
+        "promise/always-return": "off",
+        "vue/no-v-text-v-html-on-component": "warn",
+        "vue/no-setup-props-destructure": "warn",
+      }
+    },
+    {
+      "files": ["internals/**/*"],
+      "rules": {
+        "unicorn/prefer-module": "off",
+      }
+    }
+  ]
+}
+
